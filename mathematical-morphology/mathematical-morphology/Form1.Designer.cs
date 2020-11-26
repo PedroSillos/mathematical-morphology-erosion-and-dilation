97b8f0c0b97217486b0e38277608fb7a5e7f4c52
Pedro Sillos <pedro.sillos@gmail.com>
2020-11-26 15:37:41 -0300

    Interface de usuário feita

---
+++ mathematical-morphology/mathematical-morphology/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace mathematical_morphology
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_carregar_imagem = new System.Windows.Forms.Button();
+            this.pictureBox_sem_filtros = new System.Windows.Forms.PictureBox();
+            this.pictureBox_com_filtros = new System.Windows.Forms.PictureBox();
+            this.button_erosao = new System.Windows.Forms.Button();
+            this.button_Dilatacao = new System.Windows.Forms.Button();
+            this.button_achar_bordas = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_sem_filtros)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_com_filtros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_carregar_imagem
+            // 
+            this.button_carregar_imagem.AccessibleDescription = "Carregar imagem";
+            this.button_carregar_imagem.AccessibleName = "botão carregar imagem";
+            this.button_carregar_imagem.AutoSize = true;
+            this.button_carregar_imagem.Location = new System.Drawing.Point(18, 34);
+            this.button_carregar_imagem.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_carregar_imagem.Name = "button_carregar_imagem";
+            this.button_carregar_imagem.Size = new System.Drawing.Size(173, 36);
+            this.button_carregar_imagem.TabIndex = 0;
+            this.button_carregar_imagem.Text = "Carregar imagem";
+            this.button_carregar_imagem.UseVisualStyleBackColor = true;
+            this.button_carregar_imagem.Click += new System.EventHandler(this.button_carregar_imagem_Click);
+            // 
+            // pictureBox_sem_filtros
+            // 
+            this.pictureBox_sem_filtros.Location = new System.Drawing.Point(198, 19);
+            this.pictureBox_sem_filtros.Name = "pictureBox_sem_filtros";
+            this.pictureBox_sem_filtros.Size = new System.Drawing.Size(360, 360);
+            this.pictureBox_sem_filtros.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_sem_filtros.TabIndex = 1;
+            this.pictureBox_sem_filtros.TabStop = false;
+            // 
+            // pictureBox_com_filtros
+            // 
+            this.pictureBox_com_filtros.Location = new System.Drawing.Point(745, 19);
+            this.pictureBox_com_filtros.Name = "pictureBox_com_filtros";
+            this.pictureBox_com_filtros.Size = new System.Drawing.Size(360, 360);
+            this.pictureBox_com_filtros.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_com_filtros.TabIndex = 3;
+            this.pictureBox_com_filtros.TabStop = false;
+            // 
+            // button_erosao
+            // 
+            this.button_erosao.AccessibleDescription = "Erosão";
+            this.button_erosao.AccessibleName = "botão Erosão";
+            this.button_erosao.AutoSize = true;
+            this.button_erosao.Location = new System.Drawing.Point(565, 34);
+            this.button_erosao.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_erosao.Name = "button_erosao";
+            this.button_erosao.Size = new System.Drawing.Size(173, 36);
+            this.button_erosao.TabIndex = 2;
+            this.button_erosao.Text = "Erosão";
+            this.button_erosao.UseVisualStyleBackColor = true;
+            // 
+            // button_Dilatacao
+            // 
+            this.button_Dilatacao.AccessibleDescription = "Dilatação";
+            this.button_Dilatacao.AccessibleName = "botão Dilatação";
+            this.button_Dilatacao.AutoSize = true;
+            this.button_Dilatacao.Location = new System.Drawing.Point(565, 80);
+            this.button_Dilatacao.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_Dilatacao.Name = "button_Dilatacao";
+            this.button_Dilatacao.Size = new System.Drawing.Size(173, 36);
+            this.button_Dilatacao.TabIndex = 4;
+            this.button_Dilatacao.Text = "Dilatação";
+            this.button_Dilatacao.UseVisualStyleBackColor = true;
+            // 
+            // button_achar_bordas
+            // 
+            this.button_achar_bordas.AccessibleDescription = "Achar bordas";
+            this.button_achar_bordas.AccessibleName = "botão Achar bordas";
+            this.button_achar_bordas.AutoSize = true;
+            this.button_achar_bordas.Location = new System.Drawing.Point(565, 126);
+            this.button_achar_bordas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_achar_bordas.Name = "button_achar_bordas";
+            this.button_achar_bordas.Size = new System.Drawing.Size(173, 36);
+            this.button_achar_bordas.TabIndex = 5;
+            this.button_achar_bordas.Text = "Achar bordas";
+            this.button_achar_bordas.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 419);
+            this.Controls.Add(this.button_achar_bordas);
+            this.Controls.Add(this.button_Dilatacao);
+            this.Controls.Add(this.pictureBox_com_filtros);
+            this.Controls.Add(this.button_erosao);
+            this.Controls.Add(this.pictureBox_sem_filtros);
+            this.Controls.Add(this.button_carregar_imagem);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_sem_filtros)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_com_filtros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_carregar_imagem;
+        private System.Windows.Forms.PictureBox pictureBox_sem_filtros;
+        private System.Windows.Forms.PictureBox pictureBox_com_filtros;
+        private System.Windows.Forms.Button button_erosao;
+        private System.Windows.Forms.Button button_Dilatacao;
+        private System.Windows.Forms.Button button_achar_bordas;
+    }
+}
+
---
+++ mathematical-morphology/mathematical-morphology/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mathematical_morphology
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void limpar_Tela()
+        {
+            pictureBox_sem_filtros.Image = null;
+            pictureBox_com_filtros.Image = null;
+        }
+
+        private void button_carregar_imagem_Click(object sender, EventArgs e)
+        {
+            //abre o open file dialog
+            OpenFileDialog dialog_carregar_imagem = new OpenFileDialog();
+            dialog_carregar_imagem.Filter = "Image |*.png;*.jpg;*.jpeg";
+            if (dialog_carregar_imagem.ShowDialog() == DialogResult.OK)
+            {
+                limpar_Tela();
+                pictureBox_sem_filtros.Image = new Bitmap(dialog_carregar_imagem.FileName);
+            }
+        }
+    }
+}
